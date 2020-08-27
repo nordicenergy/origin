6e681dcaacfed6d99e2a83f8c917e2ebdac1a0df
Alexander Popadyuk <alexander.popadyuk@482.solutions>
2020-08-27 12:14:19 +0300

    feat(origin-ui-core): add organization already exists modal

---
+++ packages/origin-ui-core/src/components/Modal/OrganizationAlreadyExistsModal.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Dialog, DialogTitle, DialogActions, Button, Box, useTheme, Grid } from '@material-ui/core';
+import { useTranslation } from '../..';
+import iconAdded from '../../../assets/icon-org-added.svg';
+
+export const OrganizationAlreadyExistsModal = () => {
+    const [isOpen, setIsOpen] = useState(true);
+    const history = useHistory();
+
+    const showModal = isOpen;
+
+    const {
+        typography: { fontSizeMd }
+    } = useTheme();
+    const { t } = useTranslation();
+
+    const closeModal = () => {
+        setIsOpen(false);
+        history.push('/');
+    };
+
+    return (
+        <Dialog open={showModal} onClose={() => closeModal()} maxWidth={'sm'} fullWidth={true}>
+            <DialogTitle>
+                <Grid container>
+                    <Grid item xs={3}>
+                        <div style={{ position: 'relative', height: '100%', width: '100%' }}>
+                            <Box>
+                                <img
+                                    src={iconAdded}
+                                    style={{
+                                        width: '100%',
+                                        height: '100%',
+                                        position: 'absolute',
+                                        top: 0
+                                    }}
+                                />
+                            </Box>
+                        </div>
+                    </Grid>
+                    <Grid item xs>
+                        <Box px={3} mt={4}>
+                            {t('organization.registration.organizationCouldNotBeRegistered')}
+                            <Box fontSize={fontSizeMd} mt={3}>
+                                {t('organization.registration.organizationAlreadyExists')}
+                            </Box>
+                            <Box fontSize={fontSizeMd} mt={2}>
+                                {t('general.feedback.supportTeamGetInContact')}
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </DialogTitle>
+            <DialogActions>
+                <Box pr={2.5} pb={2.5}>
+                    <Button variant="contained" color="primary" onClick={() => closeModal()}>
+                        {t('general.responses.ok')}
+                    </Button>
+                </Box>
+            </DialogActions>
+        </Dialog>
+    );
+};
